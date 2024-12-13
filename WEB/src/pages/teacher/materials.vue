e7b7552e3bcb26dcb127a130c0b4d2013d9c6955
sunthorn.w <sunthorn.w@cdg.co.th>
2024-12-13 10:26:16 +0700

    fix pptx viewer

--- WEB/src/pages/teacher/materials.vue
+++ WEB/src/pages/teacher/materials.vue
@@ -56,16 +56,30 @@
       </v-col>
     </v-row>
 
-    <v-row>
+    <!-- <v-row>
       <v-col cols="12">
         <div ref="viewer"></div>
+      </v-col>
+    </v-row> -->
+    <v-row>
+      <v-col cols="12">
+        <iframe
+          ref="myIframe"
+          v-if="fileUrl"
+          :src="fileUrl"
+          width="100%"
+          height="600px"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import WebViewer from "@pdftron/webviewer";
+// import WebViewer from "@pdftron/webviewer";
+// import pptx from "../../../public/example.pptx";
 export default {
   name: "StudentMaterials",
   data() {
@@ -93,59 +107,102 @@ export default {
           type: "Student",
         },
       ],
+      fileUrl: ``,
+      // "https://docs.google.com/gview?url=http://cwestblog.com/wp-content/uploads/2020/05/Example-Presentation.pptx&embedded=true",
     };
   },
+  mounted() {},
   methods: {
+    removeElementInIframe() {
+      // Access the iframe via the ref
+      const iframe = this.$refs.myIframe;
+
+      // Ensure the iframe is accessible (same-origin policy)
+      try {
+        const iframeDoc =
+          iframe.contentDocument || iframe.contentWindow.document;
+
+        // Check if the iframe document is loaded
+        if (!iframeDoc.body) {
+          console.error("Iframe content is still blank or not loaded");
+          return;
+        }
+
+        // Find the element by ID and remove it
+        const element = iframeDoc.getElementById(
+          "ChromelessStatusBar.Options-Small14"
+        );
+        if (element) {
+          element.remove();
+          console.log("Element removed successfully");
+        } else {
+          console.log("Element not found in iframe");
+        }
+      } catch (error) {
+        console.error("Error accessing iframe content:", error);
+      }
+    },
     openDoc() {
-      WebViewer(
-        {
-          // disabledElements: ["default-top-header"],
-          path: `${process.env.BASE_URL}webviewer`,
-          initialDoc: "https://getsamplefiles.com/download/pptx/sample-2.pptx",
-        },
-        this.$refs.viewer
-      ).then((instance) => {
-        // hide the ribbons
-        instance.UI.disableElements(["default-ribbon-group"]);
-        instance.UI.disableElements(["tools-header"]);
-        instance.UI.disableElements(["leftPanelButton"]);
-        instance.UI.disableElements(["searchPanelToggle"]);
-        instance.UI.disableElements(["notesPanelToggle"]);
-        instance.UI.disableElements(["groupedLeftHeaderButtons"]);
-        instance.UI.disableElements([
-          "menuButton",
-          "panToolButton",
-          "annotationEditToolButton",
-        ]);
-        // Add header button that will get file data on click
+      this.fileUrl = `https://view.officeapps.live.com/op/embed.aspx?src=http://cwestblog.com/wp-content/uploads/2020/05/Example-Presentation.pptx`;
 
-        const { documentViewer } = instance.Core;
-
-        documentViewer.setWatermark({
-          // Draw diagonal watermark in middle of the document
-          diagonal: {
-            fontSize: 25, // or even smaller size
-            fontFamily: "sans-serif",
-            color: "red",
-            opacity: 50, // from 0 to 100
-            text: "Watermark",
-          },
-
-          // Draw header watermark
-          header: {
-            fontSize: 10,
-            fontFamily: "sans-serif",
-            color: "red",
-            opacity: 70,
-            left: "left watermark",
-            center: "center watermark",
-            right: "",
-          },
-        });
+      this.$nextTick(() => {
+        const iframe = this.$refs.myIframe;
+        if (iframe) {
+          iframe.onload = this.removeElementInIframe; // Set the onload handler
+        } else {
+          console.error("Iframe ref is not defined");
+        }
       });
+      // WebViewer(
+      //   {
+      //     // disabledElements: ["default-top-header"],
+      //     path: `${process.env.BASE_URL}webviewer`,
+      //     initialDoc: "https://getsamplefiles.com/download/pptx/sample-2.pptx",
+      //   },
+      //   this.$refs.viewer
+      // ).then((instance) => {
+      //   // hide the ribbons
+      //   instance.UI.disableElements(["default-ribbon-group"]);
+      //   instance.UI.disableElements(["tools-header"]);
+      //   instance.UI.disableElements(["leftPanelButton"]);
+      //   instance.UI.disableElements(["searchPanelToggle"]);
+      //   instance.UI.disableElements(["notesPanelToggle"]);
+      //   instance.UI.disableElements(["groupedLeftHeaderButtons"]);
+      //   instance.UI.disableElements([
+      //     "menuButton",
+      //     "panToolButton",
+      //     "annotationEditToolButton",
+      //   ]);
+      //   // Add header button that will get file data on click
+      //   const { documentViewer } = instance.Core;
+      //   documentViewer.setWatermark({
+      //     // Draw diagonal watermark in middle of the document
+      //     diagonal: {
+      //       fontSize: 25, // or even smaller size
+      //       fontFamily: "sans-serif",
+      //       color: "red",
+      //       opacity: 50, // from 0 to 100
+      //       text: "Watermark",
+      //     },
+      //     // Draw header watermark
+      //     header: {
+      //       fontSize: 10,
+      //       fontFamily: "sans-serif",
+      //       color: "red",
+      //       opacity: 70,
+      //       left: "left watermark",
+      //       center: "center watermark",
+      //       right: "",
+      //     },
+      //   });
+      // });
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<!-- <style>
+#ChromelessStatusBar.Options-Small14 {
+  display: none !important;
+}
+</style> -->
