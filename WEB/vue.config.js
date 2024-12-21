3adab1d876807e2ee7b2ec193d391a56e0f5818e
sunthorn.w <sunthorn.w@cdg.co.th>
2024-12-21 17:40:34 +0700

    fix ui

--- WEB/vue.config.js
+++ WEB/vue.config.js
@@ -1,12 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Backend server
-        changeOrigin: true,
-      },
-    },
-  },
+
 });
