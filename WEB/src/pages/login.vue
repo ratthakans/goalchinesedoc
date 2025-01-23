08aee4843844e84ed1edf0697580cf38360fd9f9
sunthorn.w <sunthorn.w@cdg.co.th>
2025-01-23 08:48:30 +0700

    fix login

--- WEB/src/pages/login.vue
+++ WEB/src/pages/login.vue
@@ -18,9 +18,9 @@
         <div class="text-center">
           <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
-          <h1 class="text-h2 font-weight-bold">
+          <div class="text-h3 font-weight-bold">
             {{ formInput?.academyName || "Academy Name" }}
-          </h1>
+          </div>
         </div>
 
         <div class="py-2" />
