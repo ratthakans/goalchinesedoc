c9acf1fd25cf4a723f8428e63b68569ea641bb87
Namfon <namfonn2369@gmail.com>
2026-01-28 18:26:32 +0700

    Fix ESLint quote error in regex

--- WEB/src/pages/admin/materials/form/material.vue
+++ WEB/src/pages/admin/materials/form/material.vue
@@ -274,7 +274,7 @@ export default {
       if (!documentPath) return "";
       const filename = documentPath.split("/").pop() || documentPath;
       // Remove timestamp suffix if present (e.g., "filename-1234567890-123456789.pdf" -> "filename.pdf")
-      return filename.replace(/-\d+-\d+(\.[^.]+)$/, '$1');
+      return filename.replace(/-\d+-\d+(\.[^.]+)$/, "$1");
     },
     onPhotoChange(file) {
       this.formInput.photo = file;
