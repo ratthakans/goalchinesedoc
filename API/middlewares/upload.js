c82b31347ab78cf0e98097d7792b0cf2269d8763
Namfon <namfonn2369@gmail.com>
2026-01-28 18:17:35 +0700

    Fix upload to preserve original filename in file path

--- API/middlewares/upload.js
+++ API/middlewares/upload.js
@@ -30,7 +30,8 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const extension = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${uniqueSuffix}${extension}`);
+    const originalName = path.basename(file.originalname, extension);
+    cb(null, `${originalName}-${uniqueSuffix}${extension}`);
   },
 });
 
