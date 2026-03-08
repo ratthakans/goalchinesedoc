069b97f1d0c55b330fd28203f4b0657dbd98c2b1
Namfon <namfonn2369@gmail.com>
2026-03-08 17:32:34 +0700

    Convert database config to PostgreSQL for Railway deployment

--- API/config/database.js
+++ API/config/database.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+    logging: false,
 });
 
 module.exports = sequelize;
